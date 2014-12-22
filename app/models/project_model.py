59e8bf086847cd29e12e7f2231fbddf2d49a1816
Daniel Wheeler <daniel.wheeler2@gmail.com>
2014-12-22 14:23:53 -0500

    Records store data in DictField
    Fix #17
    
    Records lump all the data into the DictField. This allows the data to
    have any format.
    
     * Remove serialize method as they are not needed with MongoEngine
    
     * Records reference projects and projects reference users, in one direction
    
     * GET returns correct json

--- app/models/project_model.py
+++ app/models/project_model.py
@@ -6,17 +6,5 @@ from ..models import UserModel
 class ProjectModel(db.Document):
     user = db.ReferenceField(UserModel, reverse_delete_rule=db.CASCADE)
     name = db.StringField(max_length=300, required=True)
-    #    records = db.ListField(db.EmbeddedDocumentField(RecordModel))
-
-    @property
-    def serialize(self):
-        return {'id': str(self.id),
-                'name': self.name,
-                'description': '',
-                'records': [],
-                'access': [],
-                'tags': []}
-
-
 
 
