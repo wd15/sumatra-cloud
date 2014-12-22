7582df7be4685732189a2d736b2461a4c8bbe55f
Yannick Congo <yannick.congo@gmail.com>
2014-12-22 18:11:14 -0500

    Revert "Records store data in DictField"

--- app/models/project_model.py
+++ app/models/project_model.py
@@ -6,5 +6,17 @@ from ..models import UserModel
 class ProjectModel(db.Document):
     user = db.ReferenceField(UserModel, reverse_delete_rule=db.CASCADE)
     name = db.StringField(max_length=300, required=True)
+    #    records = db.ListField(db.EmbeddedDocumentField(RecordModel))
+
+    @property
+    def serialize(self):
+        return {'id': str(self.id),
+                'name': self.name,
+                'description': '',
+                'records': [],
+                'access': [],
+                'tags': []}
+
+
 
 
--- app/models/record_model.py
+++ app/models/record_model.py
@@ -4,7 +4,11 @@ from ..models import ProjectModel
 class RecordModel(db.Document):
     project = db.ReferenceField(ProjectModel, reverse_delete_rule=db.CASCADE)
     label = db.StringField(max_length=300)
-    data = db.DictField()
-    
 
+    @property
+    def serialize(self):
+        return {'id': self.id,
+                'project_id': self.project.id,
+                'label': self.label}
 
+## add class RecordHead
